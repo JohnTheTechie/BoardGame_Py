13dc401788d5198a50b5392014f59e5bd18f1a44
JohnTheTechie <janakiraman3394@gmail.com>
2019-07-22 12:10:31 +0530

    Repository Sync trials

--- BaseFrame/base_player.py
+++ BaseFrame/base_player.py
@@ -1,7 +1,7 @@
-
+from Enumerations.States import PlayerStatus
 
 class BasePlayer:
 
-    def __init__(self, cell = None):
-        self.occupying_cell = None
-        self.status = None
+    def __init__(self, occupying_cell = None, current_status=PlayerStatus.PStatus_OUT_OF_BOARD):
+        self.occupying_cell = occupying_cell
+        self.status = current_status
